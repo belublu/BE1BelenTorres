adcceb04ca74708156e7b37d33da6fe76fdee5d9
Belen Torres <belentorress@gmail.com>
2024-06-17 22:00:29 -0300

    Creación de nuevo proyecto con correcciones

---
+++ src/routes/carts.router.js
@@ -0,0 +1,54 @@
+import express from "express";
+import CartManager from "../controllers/cart-manager.js";
+
+const router = express.Router()
+const cartManager = new CartManager("./src/models/carts.json")
+
+router.get("/", async (req, res) => {
+    try {
+        const carts = await cartManager.getCarts()
+        res.json(carts)
+    } catch (error) {
+        res.status(500).json({ error: "Error del servidor" })
+    }
+})
+
+router.get("/:cid", async (req, res) => {
+    const cartId = parseInt(req.params.cid)
+
+    try {
+        const cart = await cartManager.getCartById(cartId)
+        if (cart) {
+            res.json(cart.products)
+        } else {
+            res.status(404).json({ error: "El carrito no ha sido encontrado.", error })
+        }
+    } catch (error) {
+        res.status(500).json({ error: "Error al obtener el carrito." })
+    }
+})
+
+router.post("/", async (req, res) => {
+    try {
+        const newCart = await cartManager.createCart()
+        res.status(201).json({ id: newCart.id })
+    } catch (error) {
+        res.status(500).json({ error: "Error del servidor.", error })
+    }
+})
+
+router.post("/:cid/product/:pid", async (req, res) => {
+    const cartId = parseInt(req.params.cid);
+    const product = req.params.pid;
+    const quantity = req.body.quantity || 1;
+
+    try {
+        const updatedCart = await cartManager.addProductToCart(cartId, product, quantity);
+        res.status(201).json(updatedCart.products);
+    } catch (error) {
+        console.error("Error al agregar producto al carrito:", error);
+        res.status(500).json({ error: "Error del servidor." });
+    }
+});
+
+export default router
---
+++ src/routes/products.router.js
@@ -0,0 +1,81 @@
+import express from "express";
+import ProductManager from "../controllers/product-manager.js";
+
+const router = express.Router()
+const productManager = new ProductManager("src/models/products.json")
+
+router.get("/", async (req, res) => {
+    try {
+        const limit = req.query.limit
+        const products = await productManager.getProducts()
+        if (limit) {
+            res.json(products.slice(0, parseInt(limit)))
+        } else {
+            res.json(products)
+        }
+    } catch (error) {
+        console.error("Ha ocurrido un error al obtener los productos.", error)
+        res.status(500).json({error: "Error al obtener el producto"})
+    }
+})
+
+
+router.get("/:pid", async (req, res) => {
+    const id = req.params.pid
+
+    try {
+        const product = await productManager.getProductById(parseInt(id))
+        if(!product){
+            res.status(404).json({error: "El producto no ha sido encontrado."})
+        }
+        res.json(product)
+    } catch (error) {
+        res.status(500).json({error: "Error al obtener el producto"})
+    }
+})
+
+
+router.post("/", async (req, res) => {
+    const newProduct = req.body
+    
+    try {
+        await productManager.addProduct(newProduct)
+        res.status(201).json({message: "El producto ha sido agregado correctamente."})
+    } catch (error) {
+        res.status(500).json({error: "Error del servidor."})
+    }
+})
+
+router.put("/:pid", async (req, res) => {
+    const id = req.params.pid
+    const productUpdated = req.body
+    
+    try {
+        await productManager.updateProduct(parseInt(id), productUpdated)
+        if(productUpdated){
+            res.status(201).json("El producto se ha actualizado correctamente.")
+        }else{
+            res.status(404).json({error: "Producto no encontrado"})
+        }
+    } catch (error) {
+        res.status(500).json({error: "Error al actualizar el producto.", error})
+    }
+})
+
+router.delete("/:pid", async (req, res) => {
+    const id = req.params.pid
+    const productDeleted = req.body
+    
+    try {
+        await productManager.deleteProduct(parseInt(id), productDeleted)
+        if(productDeleted){
+            res.status(201).json("El producto se ha eliminado correctamente.")
+        }else{
+            res.status(404).json({error: "Producto no encontrado"})
+        }
+    } catch (error) {
+        res.status(500).json({error: "Error al actualizar el producto.", error})
+    }
+})
+
+export default router
